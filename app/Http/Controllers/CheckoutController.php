ef64b5ccfb544a73603ccdbd4d52f8554ba8e082
AlfaRiza <malfariza45@gmail.com>
2020-07-16 20:34:59 +0700

    Finishing email

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Auth;
 use App\Transaction;
 use App\TransactionDetail;
 use App\TravelPackage;
+use Mail;
+use App\Mail\TransactionSuccess;
 
 use Carbon\Carbon;
 
@@ -87,11 +89,17 @@ class CheckoutController extends Controller
     }
 
     public function success(Request $request, $id){
-        $transaction = Transaction::findOrFail($id);
+        $transaction = Transaction::with(['details','travel_package.galleries', 'user'])
+        ->findOrFail($id);
 
         $transaction->transaction_status = 'PENDING';
 
         $transaction->save();
+
+        // Send Email to user
+        Mail::to($transaction->user)->send(
+            new TransactionSuccess($transaction)
+        );
 
         return view('pages.success');
     }
